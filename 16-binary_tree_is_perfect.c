39f821ce2208300f28467db95b8ec28bf93aebdb
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-02-27 23:50:23 +0000

    Fix return type for 'get_depth' and refactor code

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,18 +1,43 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_count - Counts the number of number in a binary tree.
+ * binary_tree_node_count - Counts the number of nodes in a binary tree.
  * @tree: A pointer to the binary tree to count.
  *
  * Return: The number of nodes in the binary tree.
  */
-static size_t binary_tree_count(const binary_tree_t *tree)
+size_t binary_tree_node_count(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	return (1 + binary_tree_count(tree->left) +
-			binary_tree_count(tree->right));
+	return (1 + binary_tree_node_count(tree->left) +
+			binary_tree_node_count(tree->right));
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: The height of the tree is not NULL, otherwise 0.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_of_left;
+	size_t height_of_right;
+
+	if (tree == NULL)
+		return (0);
+
+	/* check the height of the left subtrees */
+	height_of_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+
+	/* check the height of the right subtrees */
+	height_of_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	/* return the maximum height amongst the two */
+	return (height_of_left > height_of_right ? height_of_left
+											 : height_of_right);
 }
 
 /**
@@ -23,33 +48,10 @@ static size_t binary_tree_count(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int depth, node_count;
-
 	if (tree == NULL)
 		return (0);
-
-	depth = binary_tree_depth(tree->left) + binary_tree_depth(tree->right);
-	node_count = binary_tree_count(tree);
 
 	/* ensure the number nodes in tree is the expected number for it's depth */
-	return (node_count == (1 << (depth + 1)) - 1);
-}
-
-/**
- * binary_tree_depth - Measures the depth of a node in a binary tree.
- * @tree: A pointer to the node to measure the depth.
- *
- * Return: The depth of the node if tree is not NULL, otherwise 0.
- */
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	size_t depth;
-
-	if (tree == NULL)
-		return (0);
-
-	/* compute the length of the path from a node back to the root */
-	depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
-
-	return (depth);
+	return ((int)binary_tree_node_count(tree) ==
+			(1 << (binary_tree_height(tree) + 1)) - 1);
 }
