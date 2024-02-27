794b5bb709e3f975e92d74c3a1f2c6f8ad4111df
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-02-27 09:32:42 +0000

    Initialize left and right pointers of new node as NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,6 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	node->n = value;
 	node->parent = parent;
+	node->left = NULL;
 	node->right = NULL;
 
 	/* handle node adjustment for non-empty left nodes */
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -23,6 +23,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
+	node->right = NULL;
 
 	/* handle node adjustment for non-empty right nodes */
 	if (parent->right != NULL)
